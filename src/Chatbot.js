514677ef4db6f5364fc1e00bcd4dcae58157f879
Shrooq713 <shroog713@gmail.com>
2023-04-12 06:35:24 +0300

    add proj

--- src/Chatbot.js
+++ src/Chatbot.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import "./Chatbot.css";
-const { Configuration, OpenAIApi } = require('openai');
+const { Configuration, OpenAIApi } = require("openai");
 const Chatbot = () => {
-  const [messages, setMessages] = useState([{
-    role: 'system',
-    content: `
+  const [messages, setMessages] = useState([
+    {
+      role: "system",
+      content: `
     You are responsible for arranging appointments at the dental clinic.
     You will check the available appointments and book the appointment if available
     
@@ -13,14 +14,15 @@ const Chatbot = () => {
     
     Ask the user if he wants to confirm the appointment, Then ask him for the triple name and mobile number. After that confirm the appointment
     `,
-},
-{
-    role: 'assistant',
-    content: 'Hello, how may I assist you with booking an appointment at the dental clinic?',
-}
-]);
+    },
+    {
+      role: "assistant",
+      content:
+        "Hello, how may I assist you with booking an appointment at the dental clinic?",
+    },
+  ]);
   const [currentMessage, setCurrentMessage] = useState("");
-  
+
   const handleChange = (event) => {
     setCurrentMessage(event.target.value);
   };
@@ -32,41 +34,48 @@ const Chatbot = () => {
     console.log(messages);
     setMessages([...messages, { role: "user", content: currentMessage }]);
     // Add logic here to process the user's message and generate a response
-    sendMessageToChatGPT()
+    sendMessageToChatGPT();
     setCurrentMessage("");
   };
   async function sendMessageToChatGPT() {
-	const configuration = new Configuration({
-		apiKey: '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',
-	});
-	const openai = new OpenAIApi(configuration);
+    const configuration = new Configuration({
+      apiKey: "api-key",
+    });
+    const openai = new OpenAIApi(configuration);
 
     console.log("messages3>>>>>>");
     console.log(messages);
-	const completion = await openai.createChatCompletion({
-		model: 'gpt-3.5-turbo',
-		messages: messages,
-		temperature: 0,
-		max_tokens: 100,
-	});
-	console.log(completion.data.choices[0].message);
-    setMessages([...messages, { role: "user", content: currentMessage },{ role: "assistant", content: completion.data.choices[0].message.content }]);
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: messages,
+      temperature: 0,
+      max_tokens: 100,
+    });
+    console.log(completion.data.choices[0].message);
+    setMessages([
+      ...messages,
+      { role: "user", content: currentMessage },
+      {
+        role: "assistant",
+        content: completion.data.choices[0].message.content,
+      },
+    ]);
     setCurrentMessage("");
-}
+  }
 
   return (
     <div className="chatbot-container">
       <div className="chatbot-messages">
-        {messages.filter((message) =>
-  message.role !== "system"
-).map((message, index) => (
-          <div
-            key={index}
-            className={message.role ==="user"? "user" : "assistant"}
-          >
-            {message.content}
-          </div>
-        ))}
+        {messages
+          .filter((message) => message.role !== "system")
+          .map((message, index) => (
+            <div
+              key={index}
+              className={message.role === "user" ? "user" : "assistant"}
+            >
+              {message.content}
+            </div>
+          ))}
       </div>
       <form className="chatbot-input-container" onSubmit={handleSubmit}>
         <input
